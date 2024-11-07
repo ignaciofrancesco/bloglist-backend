396a9fffc97b20e113d77f4c12c9b0413fa70100
ignacio <ignaciofranciscovigo@gmail.com>
2024-11-06 21:48:12 -0300

    Ex. 4.15

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
 const cors = require("cors");
+const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
+// DB connection
 mongoose.connect(MONGODB_URI);
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
---
+++ controllers/users.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const mongoose = require("mongoose");
+const User = require("../models/user");
+
+// Get all users
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  return response.json(users);
+});
+
+// Add a new user
+usersRouter.post("/", async (request, response) => {
+  const { username, password, name } = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    passwordHash,
+    name,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
+
+// Update a blog post by id
+// usersRouter.put("/:id", async (request, response) => {
+//   // Get data from the request
+//   const blogId = request.params.id;
+//   const body = request.body;
+//   const blog = {
+//     title: body.title,
+//     author: body.author,
+//     url: body.url,
+//     likes: body.likes,
+//   };
+
+//   // Process data
+//   const updatedUser = await User.findByIdAndUpdate(blogId, blog, { new: true });
+
+//   if (!updatedUser) {
+//     return response.status(404).json({ error: "Object not found." });
+//   }
+
+//   // Send response
+//   return response.json(updatedUser);
+// });
+
+// Delete a blog by id
+// usersRouter.delete("/:id", async (request, response) => {
+//   // Get data from the request
+//   const id = request.params.id;
+
+//   if (!mongoose.Types.ObjectId.isValid(id)) {
+//     return response.status(400).json({ error: "Bad request: invalid id" });
+//   }
+
+//   // Process data
+//   const result = await User.findByIdAndDelete(id);
+
+//   // Return response
+//   return response.status(204).end();
+// });
